b1cb01c1b7f415c23c1532b5db62b2984a36a970
Dnny <mcmillandanny@gmail.com>
2019-08-12 19:12:21 -0400

    making some layout changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,9 +6,48 @@ var lines = document.querySelectorAll('.lines');
 var nav = document.querySelector("nav");
 
 function hamburgerToggle() {
-    lines.forEach(function (line) {
-        line.classList.toggle("change");
-    });
+	lines.forEach(function (line) {
+		line.classList.toggle("change");
+	});
 };
 hamburger.addEventListener("click", hamburgerToggle);
+
+// Waypoints
+
+var htmlBarGraph = document.querySelector('.html-bar');
+var jsBarGraph = document.querySelector('.js-bar');
+
+var waypoint = new Waypoint({
+	element: htmlBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(htmlBarGraph, .3, {
+				width: "90%"
+			});
+		}
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+		//         width: "90%"
+		// 	});
+		// }
+	}
+});
+
+var waypoint2 = new Waypoint({
+	element: jsBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(jsBarGraph, .3, {
+				width: "75%"
+			});
+		}
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+		//         width: "90%"
+		// 	});
+		// }
+	}
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,45 @@ function hamburgerToggle() {
     });
 };
 hamburger.addEventListener("click", hamburgerToggle);
+
+
+
+// Waypoints
+
+var htmlBarGraph = document.querySelector('.html-bar');
+var jsBarGraph = document.querySelector('.js-bar');
+
+
+var waypoint = new Waypoint({
+	element: htmlBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(htmlBarGraph, .3, {
+                width: "90%"
+            });
+        }
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+        //         width: "90%"
+		// 	});
+		// }
+	}
+});
+
+var waypoint2 = new Waypoint({
+	element: jsBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(jsBarGraph, .3, {
+                width: "75%"
+            });
+        }
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+        //         width: "90%"
+		// 	});
+		// }
+	}
+});
