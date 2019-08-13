bf48f992a697577c3a6b8db650b97fee3c711165
Dnny <mcmillandanny@gmail.com>
2019-08-12 21:12:35 -0400

    addded in waypoints / styling mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,6 +16,10 @@ hamburger.addEventListener("click", hamburgerToggle);
 
 var htmlBarGraph = document.querySelector('.html-bar');
 var jsBarGraph = document.querySelector('.js-bar');
+var reactBarGraph = document.querySelector('.react-bar');
+var wordpressBarGraph = document.querySelector('.wordpress-bar');
+var vueBarGraph = document.querySelector('.vue-bar');
+var gitBarGraph = document.querySelector('.git-bar');
 
 var waypoint = new Waypoint({
 	element: htmlBarGraph,
@@ -41,6 +45,74 @@ var waypoint2 = new Waypoint({
 		if (direction === 'down') {
 			TweenMax.to(jsBarGraph, .3, {
 				width: "75%"
+			});
+		}
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+		//         width: "90%"
+		// 	});
+		// }
+	}
+});
+
+var waypoint3 = new Waypoint({
+	element: reactBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(reactBarGraph, .3, {
+				width: "65%"
+			});
+		}
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+		//         width: "90%"
+		// 	});
+		// }
+	}
+});
+
+var waypoint4 = new Waypoint({
+	element: wordpressBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(wordpressBarGraph, .3, {
+				width: "65%"
+			});
+		}
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+		//         width: "90%"
+		// 	});
+		// }
+	}
+});
+
+var waypoint5 = new Waypoint({
+	element: vueBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(vueBarGraph, .3, {
+				width: "65%"
+			});
+		}
+		// } else if (direction === "down") {
+		// 	TweenMax.to(htmlBarGraph, .3, {
+		//         width: "90%"
+		// 	});
+		// }
+	}
+});
+
+var waypoint4 = new Waypoint({
+	element: gitBarGraph,
+	offset: "80%",
+	handler: function handler(direction) {
+		if (direction === 'down') {
+			TweenMax.to(gitBarGraph, .3, {
+				width: "65%"
 			});
 		}
 		// } else if (direction === "down") {
